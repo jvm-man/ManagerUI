97cf683e33d932cf835fefffc191c92487326a96
barosanuemailtest <barosanu.email.test@gmail.com>
2020-06-07 21:50:47 +0300

    37 - WebPack implemetation

--- src/Main.ts
+++ src/Main.ts
@@ -1,12 +1,19 @@
+import { Router } from "./Router";
 
 
 
 export class Main {
 
+    private router: Router = new Router();
+
 
     public constructor() {
         console.log('Constructed new Instance of the program');
     }
+
+    public launchApp() {
+        this.router.handleRequest();
+    }
 }
 
-new Main();
+new Main().launchApp();
