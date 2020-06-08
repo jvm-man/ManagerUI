2b65a0b69df7b1a253fb0d5b04634ed5a9d1cfb4
barosanuemailtest <barosanu.email.test@gmail.com>
2020-06-08 09:45:26 +0300

    38 - First View

--- src/Router.ts
+++ src/Router.ts
@@ -1,10 +1,23 @@
+import { MainController } from "./controllers/MainController";
 
 
 export class Router {
 
 
+    private mainElement = document.getElementById('main-container');
+
     public handleRequest() {
         console.log('Handling request for:' + this.getRoute());
+
+        switch (this.getRoute()) {
+
+            default:
+                if (this.mainElement) {
+                    const mainController: MainController = new MainController();
+                    this.mainElement.append(mainController.createView());
+                }
+                break;
+        }
     }
 
     private getRoute(): string {
