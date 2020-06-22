85d7a9443d6c36ac351272f676c9dcfa7e76efce
barosanuemailtest <9tonfr9lkb97A>
2020-06-22 18:30:53 +0300

    Linting

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -6,7 +6,7 @@ export class MainController extends BaseController {
 
 
     public createView(): HTMLDivElement {
-        const title = this.createElement('h2', 'Welcome to our Main page!');
+                    const title = this.createElement('h2', 'Welcome to our Main page!');
         const article = this.createElement("div");
         article.innerText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
 
